80be272d63d51c87acc6c958d24ad256e90a18d3
Shotaro Ooki <shoutaro0726@gmail.com>
2020-06-05 16:58:14 +0900

    Commit

---
+++ api_user/views.py
@@ -0,0 +1,45 @@
+from rest_framework import generics, authentication, permissions
+from api_user import serializers
+from core.models import Profile, User
+from core.ownpermissions import ProfilePermission
+from django.db.models import Q
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from django.db import IntegrityError
+from rest_framework.exceptions import ValidationError
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = serializers.UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,ProfilePermission,)
+
+    def get_queryset(self):
+        try:
+            is_friend = Profile.objects.get(user=self.request.user)
+        except Profile.DoesNotExits:
+            is_friend = None
+        
+        friend_filter = Q()
+        for friend in is_friend.friends.all():
+            friend_filter = friend_filter | Q(user=friend)
+        
+        return self.request.filter(friend_filter)
+    
+    def perform_create(self, serializer):
+        try:
+            serializer.save(user=self.request.user)
+        except IntegrityError:
+            raise ValidationError('User can have only one own profile')
---
+++ core/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
+
+
+def upload_path(instance, filename):
+    ext = filename.split('.')[-1]
+    return '/'.join(['image'], str(instance.user.id) + str(instance.nickname) + str(".") + str(ext))
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        #email必須
+        if not email:
+            raise ValueError('Email address is must')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=50, unique=True)
+    name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    nickname = models.CharField(max_length=20)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, related_name='user', on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    friends = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='friends')
+    img = models.ImageField(blank=True, null=True, upload_to=upload_path)
+
+    def __str__(self):
+        return self.nickname
+
+class Message(models.Model):
+    message = models.CharField(max_length=200)
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='sender', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='receiver', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.message
